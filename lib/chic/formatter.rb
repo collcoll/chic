5d94d83372c808ccabf6c08205bf17eef40b83f5
Lawrance Shepstone <lawrance@collabcollective.com>
2023-12-01 10:14:00 +0100

    Adds support for context to formatters

--- lib/chic/formatter.rb
+++ lib/chic/formatter.rb
@@ -2,6 +2,13 @@
 
 module Chic
   class Formatter < SimpleDelegator
+    attr_reader :context
+
+    def initialize(object, context = nil)
+      super(object)
+      @context = context
+    end
+
     def object
       __getobj__
     end
